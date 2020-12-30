a35cf5a25420012f05c5fdd2a741bee6250eeba1
Deepak-dash007 <63140413+Deepak-dash007@users.noreply.github.com>
2020-12-31 00:07:32 +0530

    Update alert_unix.go

--- alert_unix.go
+++ alert_unix.go
@@ -4,7 +4,8 @@ package beeep
 
 // Alert displays a desktop notification and plays a beep.
 func Alert(title, message, appIcon string) error {
-	if err := Notify(title, message, appIcon); err != nil {
+	action := [][]string
+	if err := Notify(title, message, appIcon, action); err != nil {
 		return err
 	}
 	return Beep(DefaultFreq, DefaultDuration)
