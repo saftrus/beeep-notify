f777fca3a1064848d7abec3dc49b1afaa9bf9178
Deepak-dash007 <63140413+Deepak-dash007@users.noreply.github.com>
2021-01-06 15:20:09 +0530

    Update notify_windows.go

--- notify_windows.go
+++ notify_windows.go
@@ -17,7 +17,7 @@ import (
 )
 
 var isWindows10 bool
-applicationID := "Cylogic"
+var applicationID string
 
 func init() {
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
